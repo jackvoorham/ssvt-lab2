91bc5c123d64b4e46e5ff3156259ac5880de9cc5
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2022-09-14 12:53:42 +0200

    added template

---
+++ template.hs
@@ -0,0 +1,23 @@
+module ExcersiseX where
+
+import Data.List
+import Data.Char
+import System.Random
+import Test.QuickCheck
+
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+----------------------------
+
+
+
+
+
+
+
+
+
+
+-----------------------------
