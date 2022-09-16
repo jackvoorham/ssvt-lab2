c4e70f38117f73639813b4a67f788340725fd7e4
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2022-09-16 20:02:41 +0200

    IBAN parsing + checking complete

--- JackVoorham/Excersise7.hs
+++ JackVoorham/Excersise7.hs
@@ -15,13 +15,13 @@ reorderFirstFour (x : y : z : w : str) = str ++ [x] ++ [y] ++ [z] ++ [w]
 
 
 iban :: String -> Bool
-iban x = True
-
+iban str = mod asInt 97 == 1 where 
+    asInt = read $ convertCharsToInteger (reorderFirstFour str)
+    
 -- Inspired by this logic : https://stackoverflow.com/questions/20659810/haskell-int-to-char
-convertCharsToInteger :: String -> String
+convertCharsToInteger :: String -> String 
 convertCharsToInteger [] = []
 convertCharsToInteger (x : str) | ord x >= 65 && ord x <= 95 = show (ord x - 55) ++ convertCharsToInteger str
                                 | otherwise = x : convertCharsToInteger str
-
 
 -----------------------------
