04a5cf51949c7771f802b2a2f9ce701835f349c0
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2022-09-16 19:26:47 +0200

    some functions for 7 implementation

---
+++ JackVoorham/Excersise7.hs
@@ -0,0 +1,27 @@
+module Excersise7 where
+
+import Data.List
+import Data.Char
+import System.Random
+import Test.QuickCheck
+
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+----------------------------
+reorderFirstFour :: String -> String
+reorderFirstFour (x : y : z : w : str) = str ++ [x] ++ [y] ++ [z] ++ [w] 
+
+
+iban :: String -> Bool
+iban x = True
+
+-- Inspired by this logic : https://stackoverflow.com/questions/20659810/haskell-int-to-char
+convertCharsToInteger :: String -> String
+convertCharsToInteger [] = []
+convertCharsToInteger (x : str) | ord x >= 65 && ord x <= 95 = show (ord x - 55) ++ convertCharsToInteger str
+                                | otherwise = x : convertCharsToInteger str
+
+
+-----------------------------
