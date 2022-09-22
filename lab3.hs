ed3b3777b108173bd94a1a20637285c61195d8c5
Arie Bisfki <arie_bisfki@live.nl>
2022-09-22 19:48:22 +0200

    comments

--- lab3.hs
+++ lab3.hs
@@ -11,6 +11,14 @@ import Lecture3
 -- 'evl' function when I finally found out about the Lecture3.hs file.
 -- By this time I had spent around 5h or so but with the Lecture3 module I was able to implement
 -- the actual functions in an hour.
+-- As for testing, I first wrote down the 3 forms from the Lecture3 module in a more readable notation:
+-- 1. (p -> q) <-> (!q -> !p)
+-- 2. (p -> q) <-> (!p -> !q)
+-- 3. ((p -> q) ∧ (q -> r)) -> (p -> r)
+-- I drew truth tables on a piece of paper in order to know what to expect for the rules.
+-- The first rule I recognized as modus tollens, which is a tautology.
+-- The second rule I recognized as denying the antecedent, which is false for p = F and q = T.
+-- The third rule I recognized as transitivity, which is a tautology.
 -- Time spent: 360 minutes --
 
 contradiction :: Form -> Bool
@@ -19,8 +27,10 @@ contradiction f = all (\vs -> not $ evl vs f) (allVals f)
 tautology :: Form -> Bool
 tautology f = all (\vs -> evl vs f) (allVals f)
 
+-- For getting all vals, create a conjunction between f1 and f2 and then call allVals on that
 entails :: Form -> Form -> Bool
 entails f1 f2 = all (\vs -> evl vs $ Impl f1 f2) (allVals $ Cnj [f1, f2])
 
+-- For getting all vals, create a conjunction between f1 and f2 and then call allVals on that
 equiv :: Form -> Form -> Bool
 equiv f1 f2 = all (\vs -> (evl vs f1) == (evl vs f2) ) (allVals $ Cnj [f1, f2])
