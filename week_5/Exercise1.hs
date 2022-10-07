35c7768d7a2acff6fd903828409238f90bf4af87
Kyran <kyransem@gmail.com>
2022-10-07 17:20:34 +0200

    Finished ex 1

--- week_5/Exercise1.hs
+++ week_5/Exercise1.hs
@@ -3,12 +3,23 @@ import Test.QuickCheck
 import Mutation
 import MultiplicationTable
 
--- The two given mutators add elements to the list and remove elements from the list.
--- The empty output list isn't tested by these because remove elements doesn't remove all elements.
--- It also doesn't mutate to a non sorted list (reversed).
-
-emptyList :: [Integer] -> Gen [Integer]
-emptyList xs = return []
+-- The empty list is tested by anyList mutator which is able to output []. This makes this 
+-- mutator strong because it generates lists of arbitrary length.
+-- The addElements function if fairly strong since it adds elements to both ends, however it could be stronger
+-- if it was also possible for it to only add on one end.
+-- The removeElements is the same but in this case it should also remove all elements.
+-- These mutators don't cover a non sorted list so one is made.
+-- This mutator is covered by the properties.
+-- These mutators also don't cover a reversed list.
+-- This mutator is also covered by the properties.
+-- Time spend: 80 minutes --
 
 scrambleElements :: [Integer] -> Gen[Integer]
-scrambleElements xs = return $ reverse xs
+scrambleElements xs = do
+    num <- choose (1, (length xs -1))
+    let (f1, f2) = splitAt num xs
+    return $ f2 ++ f1
+
+reverseElements :: [Integer] -> Gen[Integer]
+reverseElements xs = return $ reverse xs
+
