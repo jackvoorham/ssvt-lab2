2cf0c4c0206c8b91b6741d62c0cdc3bbafef6de0
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2022-10-07 17:55:26 +0200

    .

--- week_5/Exercise2.hs
+++ week_5/Exercise2.hs
@@ -4,24 +4,23 @@ import Mutation
 import MultiplicationTable
 import Debug.Trace
 
-testAllProperties :: [[Integer] -> Integer -> Bool] -> ([Integer], Integer) -> Gen Bool
+testAllProperties :: [[Integer] -> Integer -> Bool] -> ([Integer], Integer) -> Bool
 testAllProperties (f:xs) a = if ok
                              then testAllProperties xs a 
-                             else return False
+                             else False
                            where 
                                x = fst a
                                y = snd a
                                ok = f x y 
 
-testAllProperties [] _ = return True
+testAllProperties [] _ = True
 
 countSurvivors :: Integer -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> Gen Bool 
 countSurvivors x y f = do
     let x' = fromInteger x 
     let xs = take x' [(f x, x) | x <- [1..]]
     let xs' = map (testAllProperties y) xs
-    let bools = map (\x -> x >= x) xs
-    let ok = all (==True) bools 
+    let ok = all (==True) xs' 
     return ok
     
 test = countSurvivors 4000 multiplicationTableProps multiplicationTable  
