a1f4211d3309dacfd92d47205c2602233fba7144
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2022-10-07 17:40:06 +0200

    save

--- week_5/Exercise2.hs
+++ week_5/Exercise2.hs
@@ -4,21 +4,24 @@ import Mutation
 import MultiplicationTable
 import Debug.Trace
 
-testAllProperties :: [[Integer] -> Integer -> Bool] -> ([Integer], Integer) -> Bool
-testAllProperties (f:xs) a | ok = testAllProperties xs a 
-                           | otherwise = False
+testAllProperties :: [[Integer] -> Integer -> Bool] -> ([Integer], Integer) -> Gen Bool
+testAllProperties (f:xs) a = if ok
+                             then testAllProperties xs a 
+                             else return False
                            where 
                                x = fst a
                                y = snd a
                                ok = f x y 
-                               
-testAllProperties [] _ = True
 
-countSurvivors :: Integer -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> Bool 
-countSurvivors x y f = ok where
-    x' = fromInteger x 
-    xs = take x' [(f x, x) | x <- [1..]]
-    xs' = map (testAllProperties y) xs
-    ok = all (==True) xs'
+testAllProperties [] _ = return True
+
+countSurvivors :: Integer -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> Gen Bool 
+countSurvivors x y f = do
+    let x' = fromInteger x 
+    let xs = take x' [(f x, x) | x <- [1..]]
+    let xs' = map (testAllProperties y) xs
+    let bools = map (\x -> x >= x) xs
+    let ok = all (==True) bools 
+    return ok
     
 test = countSurvivors 4000 multiplicationTableProps multiplicationTable  
