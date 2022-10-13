1f2cb29bfad4a0eb034dbbdac16b294473dbc5f4
Kyran <kyransem@gmail.com>
2022-10-13 18:11:44 +0200

    Update Excersise2.hs

--- week_6/Excersise2.hs
+++ week_6/Excersise2.hs
@@ -6,8 +6,17 @@ import Test.QuickCheck
 -- The union function is already given in the setOrd file. Because it is not clear whether we should
 -- implement all the functions ourselves, we decided to do both.
 -- The normal function is our own implementation and the setUnion' function uses the given union function.
+-- In order to test we had to write our own generator to use with quickcheck which took a considerable amount of time.
+-- To test intersect we test if the intersect is a subset of both sets.
+-- To test union we test is both sets are subsets of the union.
+-- To test the difference we need to first test the edge cases:
+-- If the first set is empty or if the first set is a subset of the second set,
+-- the difference will be the empty set which will always be a subset of the second set.
+-- In that case we can simply test if the difference is the empty set.
+-- In other cases we test if the difference is a subset of set 1 but not of set 2.
+-- Time spend: 250 minutes --
 
-setIntersection ::   Ord a => Set a -> Set a -> Set a
+setIntersection :: Ord a => Set a -> Set a -> Set a
 setIntersection (Set []) set2 = Set []
 setIntersection (Set (x:xs)) set2
     | inSet x set2 = insertSet x (setIntersection (Set xs) set2)
@@ -40,5 +49,24 @@ propIntersect :: Set Int -> Set Int -> Bool
 propIntersect set1 set2 = and [subSet intersect set1, subSet intersect set2]
   where intersect = setIntersection set1 set2
 
+propUnion :: Set Int -> Set Int -> Bool
+propUnion set1 set2 = and [subSet set1 union, subSet set2 union]
+  where union = setUnion set1 set2
+
+propUnion' :: Set Int -> Set Int -> Bool
+propUnion' set1 set2 = and [subSet set1 union, subSet set2 union]
+  where union = setUnion' set1 set2
+
+propDifference :: Set Int -> Set Int -> Bool
+propDifference set1 set2 
+  | isEmpty set1 = isEmpty (setDifference set1 set2)
+  | subSet set1 set2 = isEmpty (setDifference set1 set2)
+  | otherwise = and [subSet difference set1, not (subSet difference set2)]
+      where difference = setDifference set1 set2
+
 main :: IO()
-main = do quickCheck propIntersect
+main = do 
+  quickCheck propIntersect
+  quickCheck propUnion
+  quickCheck propUnion'
+  quickCheck propDifference
