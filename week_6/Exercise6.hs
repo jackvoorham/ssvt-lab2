c9d1f41f44a18976191cac609fe8415eb87af03f
Arie Bisfki <arie_bisfki@live.nl>
2022-10-13 23:28:03 +0200

    ex6 comments

--- week_6/Exercise6.hs
+++ week_6/Exercise6.hs
@@ -2,6 +2,16 @@ module Exercise6 where
 import Test.QuickCheck
 import Exercise3
 
+-- I noticed first that the Rel type has a structure that is suitable for use with haskell function 'lookup'.
+-- However the 'lookup' function is not exactly suitable here because it returns only the first match. So
+-- I wrote lookupAll which returns all matches. This allows propSymClos to check if, from a given pair (k,v)
+-- from a relationship,
+-- pair (v,k) also exists, in which the symmetric property for that pair would hold.
+-- Time spent: 90m
+-- Test report:
+-- +++ OK, passed 100 tests.
+-- +++ OK, passed 100 tests.
+
 lookupAll :: Eq a => a -> [(a, b)] -> [b]
 lookupAll key [] = []
 lookupAll key ((k, v):rest) = iterationResult ++ (lookupAll key rest)
@@ -14,9 +24,6 @@ propSymClos rel = propSymClos' rel symClosOfRel
 propSymClos' :: Ord a => Rel a -> Rel a -> Bool
 propSymClos' [] symClosOfRel = True
 propSymClos' ((k,v):rest) symClosOfRel = if k `elem` (lookupAll v symClosOfRel) then propSymClos' rest symClosOfRel else False
-
-propTrClos' :: Ord a => Rel a -> Rel a -> Bool
-propTrClos' (entry:rest) trClosOfRel =
 
 main :: IO()
 main = do
