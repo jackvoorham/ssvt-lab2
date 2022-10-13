d050d87be8628d31c37f87e272d9ddcefa5ac727
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2022-10-13 22:36:20 +0200

    4.1/4.2

--- week_6/Excersise4.hs
+++ week_6/Excersise4.hs
@@ -1,19 +1,62 @@
 module Excersise4 where
 import SetOrd
+import Data.List
+import Control.Monad
+import Test.QuickCheck
 
 type Rel a = [(a,a)]
 
--- A relation R is serial on a domain A if for any x ∈ A there is an y ∈ A such that xRy. Suppose relations are represented as lists of pairs:
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+ 
+cartProd :: Arbitrary a => [a] -> [a] -> Gen [(a,a)]
+cartProd xs ys = do return [(x,y) | x <- xs, y <- ys]
+
+arbitraryReflexive :: Gen ([Int], Rel Int)
+arbitraryReflexive = do
+    len <- chooseInt (1, 10)
+    d <- vector len >>= \x -> return (nub x) 
+    let rel = nub (zip d d)
+    return (d, rel)
+    
+arbitraryDomainRelation :: Gen ([Int], Rel Int)
+arbitraryDomainRelation = do
+    dLen <- chooseInt (1, 10)
+    rLen <- chooseInt (1, 10)
+    d <- vector dLen >>= \x -> return (nub x) 
+    rel <- cartProd d d >>= (shuffle Control.Monad.>=> (return . take rLen))
+    return (d, rel)
 
 checkRelation :: Eq a => a -> [a] -> Rel a -> Bool
 checkRelation x d r = any (`elem` r) ([(x, y) | y <- d])
 
+checkAllFirst :: Eq a => [a] -> Rel a -> Bool 
+checkAllFirst d (x : rel) | fst x `elem` d = checkAllFirst d rel
+                          | otherwise = False
+
+checkAllFirst _ [] = True
+
 isSerial :: Eq a => [a] -> Rel a -> Bool 
-isSerial x y = all ((== True) . (\ z -> checkRelation z x y)) x
+isSerial d rel = checkAllFirst d rel && all ((== True) . (\ z -> checkRelation z d rel)) d
 
--- Should be false
-checkFalse = isSerial [1,2,3] [(1,3), (1,2), (2,3)]
+propCheckLength :: Eq a => ([a], Rel a) -> Property 
+propCheckLength (d, rel) = isSerial d rel ==> length rel >= length d
 
--- Should be true
-checkTrue = isSerial [1,2,3] [(1,3), (1,2), (2,3), (3,1)]
- 
+checkIsSerial :: Eq a => ([a], Rel a) -> Bool
+checkIsSerial (d, rel) = isSerial d rel
+
+main = do
+    putStrLn "Property 1: isSerial always returns true on reflexive relations."
+    quickCheck $ forAll arbitraryReflexive checkIsSerial
+    putStrLn "Property 2: length of relation is always greater or equal to the length of domain."
+    quickCheck $ forAll arbitraryDomainRelation propCheckLength
+
+-- Time spent: 180 minutes --
+-- Test report:
+-- Property 1: isSerial always returns true on reflexive relations.
+-- +++ OK, passed 100 tests.
+-- Property 2: length of relation is always greater or equal to the length of domain.
+-- +++ OK, passed 100 tests; 193 discarded.
+
+-- Todo: 4.3 and comments
